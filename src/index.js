201fba14089d3defc21dccc0260c6d7cd992ab12
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-16 17:28:37 +0200

    Pruebas de conexión a BBDD

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { checkDB, syncDB } from './config/db.js';
+import router from './routes/router.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+app.use('/', router);
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+checkDB();
+syncDB();
+
+app.listen(PORT, () => {
+    console.log(`Servidor conectado correctamente por el puerto ${PORT}`)
+})
---
+++ src/routes/categoryRouter.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import categoryController from "../controllers/api/categoryController.js";
+import functions from "../controllers/api/categoryController.js";
+
+const router = Router();
+
+//router.get("/", categoryController.getAllCategories);
+
+export default categoryRouter;
