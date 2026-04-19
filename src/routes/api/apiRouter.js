1190e354d767a537e71cb34088967a811cf8435f
ane1212 <aneee121212@gmail.com>
2026-04-19 14:13:23 +0200

    Creado el controlador de usuario y los endpoints necesarios para el CRUD

--- src/routes/api/apiRouter.js
+++ src/routes/api/apiRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import categoryRouter from "../categoryRouter.js";
+import userRouter from "./userRouter.js";
 
 const apiRouter = Router();
 apiRouter.use('/category', categoryRouter);
+apiRouter.use('/user', userRouter)
 
 export default apiRouter;
