0f45506bdfe631af8f535abf3f9a634e2f47f785
ane1212 <aneee121212@gmail.com>
2026-04-20 15:54:18 +0200

    Creación de estructura de las vistas del dashboard y principal

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { checkDB, syncDB } from './config/db.js';
 import router from './routes/router.js';
+import expressEjsLayouts from 'express-ejs-layouts';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use(expressEjsLayouts);
+app.use(express.static("public"));
+
 app.use(express.urlencoded());
 app.use(express.json());
 
 app.use('/', router);
+
+app.get('/', (req, res) => {
+    res.render('pages/index', { layout: 'layouts/main' });
+});
+
+app.get('/admin', (req, res) => {
+    res.render('dashboard/index', { layout: 'layouts/dashboard' });
+});
 
 checkDB();
 syncDB();
