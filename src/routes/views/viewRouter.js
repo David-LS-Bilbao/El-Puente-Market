f6ea06bf19ab96579928628da28c504537fe6ca5
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-23 01:13:42 +0200

    Añadir ruta del carrito

--- src/routes/views/viewRouter.js
+++ src/routes/views/viewRouter.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import productViewRouter from "./productViewRouter.js";
 import userViewRouter from "./userViewRouter.js";
 import categoryViewRouter from './categoryViewRouter.js';
+import cartViewRouter from "./cartViewRouter.js";
 
 const viewRouter = Router();
 viewRouter.use('/admin/user', userViewRouter);
 viewRouter.use("/category", categoryViewRouter);
+viewRouter.use("/cart", cartViewRouter);
 viewRouter.use("/", productViewRouter);
 
 export default viewRouter;
