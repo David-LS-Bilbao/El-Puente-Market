1428f81e4d2ffc7695ac27d37969bce4e39bea24
ane1212 <aneee121212@gmail.com>
2026-04-22 22:56:57 +0200

    Creadas las vistas de crear usuario, editar usuario y detalles usuario, también se han creado las rutas de las vistas  correspondientes

--- src/controllers/views/userViewController.js
+++ src/controllers/views/userViewController.js
@@ -6,53 +6,51 @@ async function getAllUsers(req, res) {
 };
 
 async function getUserByDNI(req, res) {
-    try {
-        const dni = req.params.dni;
-        const user = await userService.getUserByDNI(dni);
-        res.render("", { user, layout: "layouts/dashboard" });
+    const dni = req.params.dni;
+    const user = await userService.getUserByDNI(dni);
+    res.render("dashboard/editUser", { user, layout: "layouts/dashboard" });
 
-    } catch (error) {
-        parseError(error, res);
-    }
 }
 
 async function createUserRegister(req, res) {
-    try {
-        const user = await userService.createUserRegister(req.body);
-        res.render("dashboard/createUser", { user, layout: "layouts/dashboard" });
-
-    } catch (error) {
-        parseError(error, res);
-    }
-
+    const user = await userService.createUserRegister(req.body);
+    return res.redirect('/admin/user');
 }
 
+
 async function updateUser(req, res) {
-    try {
-        const dni = req.params.dni;
-        const user = await userService.updateUser(dni, req.body);
-        res.render("dashboard/editUser", { user, layout: "layouts/dashboard" });
-
-    } catch (error) {
-        parseError(error, res);
-    }
-
+    const dni = req.params.dni;
+    const user = await userService.updateUser(dni, req.body);
+    res.redirect('/admin/user');
 }
 
 async function deleteUser(req, res) {
-    try {
-        const dni = req.params.dni;
-        const user = await userService.deleteUser(dni);
-        res.json(user);
-    } catch (error) {
-        parseError(error, res);
-    }
+    const dni = req.params.dni;
+    const user = await userService.deleteUser(dni);
+    res.redirect('/admin/user');
 }
+
 async function getViewCreateUser(req, res) {
     res.render('dashboard/createUser', {
         layout: 'layouts/dashboard'
     });
 }
 
-export const functions = { getAllUsers, getUserByDNI, createUserRegister, updateUser, deleteUser, getViewCreateUser };
+async function getViewEditUser(req, res) {
+    res.render('dashboard/editUser', {
+        layout: 'layouts/dashboard'
+    });
+}
+
+async function getUserDetail(req, res) {
+    const dni = req.params.dni;
+    const user = await userService.getUserByDNI(dni);
+
+    return res.render("dashboard/userDetail", {
+        user,
+        layout: "layouts/dashboard"
+    });
+}
+
+export const functions = { getAllUsers, getUserByDNI, createUserRegister, updateUser, deleteUser, getViewCreateUser, getViewEditUser, getUserDetail };
 export default functions;
--- src/routes/views/userViewRouter.js
+++ src/routes/views/userViewRouter.js
@@ -4,11 +4,12 @@ import functions from "../../controllers/views/userViewController.js";
 
 const userViewRouter = Router();
 userViewRouter.get("/", functions.getAllUsers);
+userViewRouter.get('/create', functions.getViewCreateUser);
+userViewRouter.get('/edit/:dni', functions.getUserByDNI);
+userViewRouter.get('/details/:dni', functions.getUserDetail);
 userViewRouter.get("/:dni", functions.getUserByDNI);
-userViewRouter.get("/user/create/", functions.createUserRegister);
 userViewRouter.post("/", functions.createUserRegister);
-userViewRouter.put("/:dni", functions.updateUser);
-userViewRouter.delete("/:dni", functions.deleteUser);
+userViewRouter.post("/:dni", functions.updateUser);
+userViewRouter.post("/delete/:dni", functions.deleteUser);
 
-userViewRouter.get('/admin/users/create', functions.getViewCreateUser);
 export default userViewRouter;
