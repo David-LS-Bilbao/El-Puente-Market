44038d6cd3c638f1c9d1089603064ce6e7d933dc
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-22 19:39:45 +0200

    Arreglar problema de obtener vista del formulario del nuevo usuario en la ruta /admin/user/create

--- src/routes/views/userViewRouter.js
+++ src/routes/views/userViewRouter.js
@@ -4,11 +4,11 @@ import functions from "../../controllers/views/userViewController.js";
 
 const userViewRouter = Router();
 userViewRouter.get("/", functions.getAllUsers);
+userViewRouter.get('/create/', functions.getViewCreateUser);
 userViewRouter.get("/:dni", functions.getUserByDNI);
 userViewRouter.get("/user/create/", functions.createUserRegister);
 userViewRouter.post("/", functions.createUserRegister);
 userViewRouter.put("/:dni", functions.updateUser);
 userViewRouter.delete("/:dni", functions.deleteUser);
 
-userViewRouter.get('/admin/users/create', functions.getViewCreateUser);
 export default userViewRouter;
