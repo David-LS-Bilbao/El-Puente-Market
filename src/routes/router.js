154cab43f7584d0fe29b2b712d8c61d2b09b71b1
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-26 22:23:59 +0200

    Eliminar categoryRouter porque no se usa

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import apiRouter from "./api/apiRouter.js";
 import viewRouter from "./views/viewRouter.js";
-import categoryRouter from "./categoryRouter.js";
 import cartViewRouter from "./views/cartViewRouter.js";
 
 const router = Router();
