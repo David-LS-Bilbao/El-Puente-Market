6ae98971e0f49ede1d7f473966bdfaba6be51524
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-26 22:27:16 +0200

    Eliminar middleware

--- src/routes/views/productViewRouter.js
+++ src/routes/views/productViewRouter.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import productViewController from "../../controllers/views/productViewController.js";
-import productMiddlewares from "../../middlewares/productMiddleware.js";
 
 const productViewRouter = Router();
 
 productViewRouter.get("/", productViewController.getAllProductsAndCart);
-productViewRouter.get("/category/:id", productViewController.getProductsByCategory);
 productViewRouter.get("/:id", productViewController.getProductById);
+productViewRouter.get("/category/:id", productViewController.getProductsByCategory);
 
 productViewRouter.post("/", productViewController.addNewProduct);
 productViewRouter.put("/:id", productViewController.changeProduct);
