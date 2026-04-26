54c8173b8a27aa9e67f9a063166beb66dcbd6ccb
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-26 21:30:46 +0200

    Eliminar middlewares y renombrar productViewController

--- src/routes/views/productViewRouter.js
+++ src/routes/views/productViewRouter.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import functions from "../../controllers/views/productViewController.js";
+import productViewController from "../../controllers/views/productViewController.js";
 import productMiddlewares from "../../middlewares/productMiddleware.js";
 
 const productViewRouter = Router();
 
-productViewRouter.get("/", functions.getAllProducts);
-productViewRouter.get("/category/:id", productMiddlewares.checkProductId, functions.getProductsByCategory);
-productViewRouter.get("/:id", functions.getProductById);
+productViewRouter.get("/", productViewController.getAllProductsAndCart);
+productViewRouter.get("/category/:id", productViewController.getProductsByCategory);
+productViewRouter.get("/:id", productViewController.getProductById);
 
-productViewRouter.post("/", productMiddlewares.checkNewProduct, functions.addNewProduct);
-productViewRouter.put("/:id", productMiddlewares.checkProductId, productMiddlewares.checkProduct, functions.changeProduct);
-productViewRouter.patch("/:id", productMiddlewares.checkProductId, functions.changeProductField);
-productViewRouter.delete("/:id", productMiddlewares.checkProductId, functions.deleteProduct);
+productViewRouter.post("/", productViewController.addNewProduct);
+productViewRouter.put("/:id", productViewController.changeProduct);
+productViewRouter.patch("/:id", productViewController.changeProductField);
+productViewRouter.delete("/:id", productViewController.deleteProduct);
 
 export default productViewRouter;
