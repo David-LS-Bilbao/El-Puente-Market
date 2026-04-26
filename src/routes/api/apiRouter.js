5f55be2005eb18a5920f4cb64c1826d6da97b0d7
Jon Aldekoa <jaldekoa@gmail.com>
2026-04-26 22:25:17 +0200

    Modificar importación del categoryRouter

--- src/routes/api/apiRouter.js
+++ src/routes/api/apiRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import categoryRouter from "../categoryRouter.js";
+import categoryRouter from "./categoryRouter.js";
 import cartRouter from "./cartRouter.js";
 import userRouter from "./userRouter.js";
 import productRouter from "./productRouter.js"
@@ -8,6 +8,6 @@ const apiRouter = Router();
 apiRouter.use('/category', categoryRouter);
 apiRouter.use('/product', productRouter);
 apiRouter.use('/user', userRouter)
-apiRouter.use('/cart',cartRouter);
+apiRouter.use('/cart', cartRouter);
 
 export default apiRouter;
